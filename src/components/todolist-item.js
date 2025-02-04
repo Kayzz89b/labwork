e7773622c395c47be3e1d0bd16a00ff8763e07c5
Kayzz89b <130134664+Kayzz89b@users.noreply.github.com>
2025-02-04 12:24:57 +0000

    step 12

--- src/components/todolist-item.js
+++ src/components/todolist-item.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import './todolist-item.css';
 
 const TodoListItem = ({ label, important = false }) => {
 
     const style = {
         color: important ? 'tomato' : 'black'
     };
-    return <span style={ style }>{ label }</span>
+    return <span className={ "todo-list-item" }
+    style={ style }> { label }
+</span>;
 };
 export default TodoListItem;
