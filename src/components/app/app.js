fc9fda629f679b7de9c73b6f9e64bd29d9df695b
Kayzz89b <130134664+Kayzz89b@users.noreply.github.com>
2025-02-12 04:05:13 +0000

    step 26

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,14 @@
-
 import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
 export  default  class  App extends Component {
 
-    
     state = {
      todoData : [
             { label: 'Drink Coffee', important: false, id: 1 },
@@ -18,24 +17,22 @@ export  default  class  App extends Component {
         ]
     };
 
-    
     deleteItem = (id) => {
         this.setState(({todoData} ) => {
             const idx = todoData.findIndex((element) => element.id === id);
             // console.log(idx);
-            todoData.splice(id, 1);
-            // [a, b c, d, e]
-            // [a, b,   d, e]
-            const before = todoData.slice(0, idx);
-            const after = todoData.slice(idx +1);
-            const newArray = [...before, ...after];
+            
+                const newArray = [
+                ...todoData.slice(0, idx),
+                ...todoData.slice(idx +1)
+            ];
+
             return{
                 todoData: newArray
             };
         });
     };
 
-           
     render() {
         return (
             <div className="todo-app">
